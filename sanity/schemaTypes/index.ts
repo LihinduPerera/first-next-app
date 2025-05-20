b5f015807c8964562a4255413da0f750073f9daf
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-05-20 13:44:59 +0530

    feat: add author and avengers schema types; integrate markdown support

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author } from './auther'
+import { avengers } from './avengers'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author , avengers],
 }
